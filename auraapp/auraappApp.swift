df960c58514ff74e555529f895f7db14e2e85a64
irfan vural <irfanvural.dev@gmail.com>
2026-03-12 14:46:27 +0300

    Initial Commit

---
+++ auraapp/auraappApp.swift
@@ -0,0 +1,17 @@
+//
+//  auraappApp.swift
+//  auraapp
+//
+//  Created by İrfan Vural on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct auraappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
